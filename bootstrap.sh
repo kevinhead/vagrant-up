d995a050542ffedf1944bb1d37bf901744839613
Kevin Head <kevinleehead@gmail.com>
2018-10-13 18:46:07 -0500

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+TERRAFORM_VERSION=0.11.8
+TERRAFORM_SHA256SUM=84ccfb8e13b5fce63051294f787885b76a1fedef6bdbecf51c5e586c9e20c9b7
+
+apt-get install -y unzip \
+                   curl \
+                   git \
+                   apache2
+
+curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip > terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
+    echo "${TERRAFORM_SHA256SUM}  terraform_${TERRAFORM_VERSION}_linux_amd64.zip" > terraform_${TERRAFORM_VERSION}_SHA256SUMS && \
+    sha256sum --check --strict terraform_${TERRAFORM_VERSION}_SHA256SUMS && \
+    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /bin && \
+    rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
